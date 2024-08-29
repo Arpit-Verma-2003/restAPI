6124bb62ebe28efb2a521849459128ff2f84bdc2
Arpit-Verma-2003 <arpitverma200316@gmail.com>
2024-08-29 16:03:16 +0530

    delete&update

--- src/students/controller.js
+++ src/students/controller.js
@@ -6,27 +6,53 @@ const getStudents = (req, res) => {
     else res.status(200).json(results.rows);
   });
 };
-const getStudentsById = (req,res) =>{
-    const id = parseInt(req.params.id);
-    pool.query(queries.getStudentsById,[id],(err,results)=>{
-        if(err) throw err;
-        else res.status(200).json(results.rows);
-    })
-}
-const addStudents = (req,res) =>{
-    const {name,email,age,dob} = req.body;
-    // logic to email duplicate check
-    pool.query(queries.checkMailExists,[email],(err,results)=>{
-        if(results.rows.length) return res.end("Email Already Exists");
+const getStudentsById = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentsById, [id], (err, results) => {
+    if (err) throw err;
+    else res.status(200).json(results.rows);
+  });
+};
+const addStudents = (req, res) => {
+  const { name, email, age, dob } = req.body;
+  // logic to email duplicate check
+  pool.query(queries.checkMailExists, [email], (err, results) => {
+    if (results.rows.length) return res.end("Email Already Exists");
 
-        pool.query(queries.addStudents,[name,email,age,dob],(err,results)=>{
-            if(err) throw err;
-            else res.status(201).send("student created successfully");
-        })
-    })
-}
+    pool.query(queries.addStudents, [name, email, age, dob], (err, results) => {
+      if (err) throw err;
+      else res.status(201).send("student created successfully");
+    });
+  });
+};
+
+const removeStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query(queries.getStudentsById, [id], (err, results) => {
+    if (!results.rows.length) return res.end("No User Found | Can't Delete");
+    pool.query(queries.removeStudentsById, [id], (err, results) => {
+      if (err) throw err;
+      else res.status(200).send("Deleted Successfully");
+    });
+  });
+};
+
+const updateStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
+  pool.query(queries.getStudentsById, [id], (err, results) => {
+    if (!results.rows.length) return res.end("No User Found | Can't Delete");
+    pool.query(queries.updateStudentById, [name,id], (err, results) => {
+      if (err) throw err;
+      return res.status(200).send("Updated Successfully");
+    });
+  });
+};
+
 module.exports = {
   getStudents,
   getStudentsById,
-  addStudents
+  addStudents,
+  removeStudent,
+  updateStudent,
 };
--- src/students/queries.js
+++ src/students/queries.js
@@ -2,9 +2,13 @@ const getStudents = "SELECT * FROM students";
 const getStudentsById = "SELECT * FROM students WHERE id = $1";
 const checkMailExists = "SELECT s FROM students s WHERE s.email = $1";
 const addStudents = "INSERT INTO students (name,email,age,dob) VALUES($1,$2,$3,$4)";
+const removeStudentsById = "DELETE FROM students WHERE id = $1";
+const updateStudentById = "UPDATE students SET name = $1 where id = $2"
 module.exports = {
   getStudents,
   getStudentsById,
   checkMailExists,
-  addStudents
+  addStudents,
+  removeStudentsById,
+  updateStudentById,
 };
